c4f8b48073f5d487f0af519f09bae4b684b4fa0e
rodrigohaddad <rgh@poli.ufrj.br>
2022-06-09 22:11:15 -0300

    graph transformation

--- graph_transformation/transformation.py
+++ graph_transformation/transformation.py
@@ -1,9 +1,25 @@
 import networkx as nx
 
 
+class NodeMetrics:
+    def __init__(self,
+                 hood_infection,
+                 alpha_numerator,
+                 alpha_denominator):
+        self.eta = self._calculate_eta(hood_infection, alpha_denominator)
+        self.alpha = self._calculate_alpha(alpha_numerator, alpha_denominator)
+
+    @classmethod
+    def _calculate_eta(cls, h_inf, a_den):
+        return [d and n / d or 0 for n, d in zip(h_inf, a_den)]
+
+    @classmethod
+    def _calculate_alpha(cls, a_num, a_den):
+        return [d and n / d or 0 for n, d in zip(a_num, a_den)]
+
+
 class GraphTransform:
-    nodes_alphas = dict()
-    nodes_etas = dict()
+    nodes_metrics = dict()
     G_new = None
 
     def __init__(self, g_inf, k, alpha_weight):
@@ -32,8 +48,9 @@ class GraphTransform:
                 n_inf = self._calculate_neighbourhood_infection(v)
                 neighbors_infection[distance] += n_inf
 
-            self.nodes_etas[u] = [d and n / d or 0 for n, d in zip(neighbors_infection, alpha_denominator)]
-            self.nodes_alphas[u] = [d and n / d or 0 for n, d in zip(alpha_numerator, alpha_denominator)]
+            self.nodes_metrics[u] = NodeMetrics(neighbors_infection,
+                                                alpha_numerator,
+                                                alpha_denominator)
 
     def _calculate_neighbourhood_infection(self, v):
         n_inf = 0
@@ -44,13 +61,15 @@ class GraphTransform:
     def _calculate_node_infection_index(self, ring_index, neighbor_index):
         return self.alpha_weight*ring_index+(1-self.alpha_weight)*neighbor_index
 
-    def _calculate_infection_similarity(self, nodes_inf):
-        pass
+    def _calculate_infection_similarity(self):
+        weights = []
+        for u, u_metrics in self.nodes_metrics.items():
+            for v, v_metrics in self.nodes_metrics.items():
+                weights.append((u, v,))
 
     def _calculate_metrics(self):
         self._calculate_ring_infection()
-        self._calculate_infection_similarity(self.nodes_alphas)
-        self._calculate_infection_similarity(self.nodes_etas)
+        self._calculate_infection_similarity()
 
     def _create_new_graph(self):
         self.G_new.add_edges_from([(1, 2, {'weight': 0.3}), (1, 3, {'weight': 0.3})])
