6934cea88a86d52c4dfef961e576918f864a11fa
rodrigohaddad <rgh@poli.ufrj.br>
2022-11-07 11:30:10 -0300

    Plot line graph comparison

--- plot_line_graph.py
+++ plot_line_graph.py
@@ -1,22 +1,43 @@
+import json
+
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
+METRICS_AGGREGATED_FILES = ['metrics_output_train.json', 'metrics_output_train_nb.json',
+                            'metrics_output_test.json', 'metrics_output_test_nb.json']
+
 
 def main():
-    aa = {'n_sources': [1, 1], 'top_k': [1, 2], 'precision_mean': [0.5, 0.5], 'recall_mean': [0.5, 0.6], 'infection_percentage': ['enriched', 'enriched'], 'f_score_mean': [0.5, 0.5333333373069763]}
+    sns.set_style("whitegrid")
+    for file_path in METRICS_AGGREGATED_FILES:
+        f = open(f'data/{file_path}', 'r')
+        data = pd.DataFrame(json.load(f))
+        data_preproc_inf = data.where(data['infection_percentage'] == 5)
+        data_preproc_src = data.where(data['n_sources'] == 3)
 
-    data_preproc = pd.DataFrame({
-        'Year': [1, 2, 3, 4, 5],
-        'A': [1, 1, 2, 4, 5],
-        'B': [1, 2, 5, 4, 5],
-        'C': [1, 2, 3, 4, 5],
-        'D': [1, 2, 1, 4, 8]})
+        for s, group in data_preproc_inf.groupby(['n_sources']):
+            fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(24, 5))
+            sns.lineplot(x='top_k', y='precision_mean', data=group, ax=ax1, marker="o", linestyle='--', color='black')
+            sns.lineplot(x='top_k', y='recall_mean', data=group, ax=ax2, marker="o", linestyle='--', color='black')
+            sns.lineplot(x='top_k', y='f_score_mean', data=group, ax=ax3, marker="o", linestyle='--', color='black')
+            ax1.set_title(f'Precision')
+            ax2.set_title(f'Recall')
+            ax3.set_title(f'F-score')
+            fig.suptitle(
+                f"{file_path.split('_')[2].split('.')[0]} - {int(s)} sources - 5% infection{' - Neighbors' if file_path.split('_')[-1].split('.')[0] == 'nb' else ''}")
+            plt.savefig(f"data/figures/comparison/comparison_{int(s)}s_5inf_{file_path.split('_')[2].split('.')[0]}{'_nb' if file_path.split('_')[-1].split('.')[0] == 'nb' else ''}", dpi=120)
 
-    axes = sns.lineplot(x='Year', y='value', hue='variable',
-                        data=pd.melt(data_preproc, ['Year']))
-    axes.set_title(f'Precision M:{data_preproc["A"].mean():.4f} V:{data_preproc["A"].var():.4f}')
-    plt.savefig("data/figures/comparison/comparison_1", dpi=120)
+        for inf, group in data_preproc_src.groupby(['infection_percentage']):
+            fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(24, 5))
+            sns.lineplot(x='top_k', y='precision_mean', data=group, ax=ax1, marker="o", linestyle='--', color='black')
+            sns.lineplot(x='top_k', y='recall_mean', data=group, ax=ax2, marker="o", linestyle='--', color='black')
+            sns.lineplot(x='top_k', y='f_score_mean', data=group, ax=ax3, marker="o", linestyle='--', color='black')
+            ax1.set_title(f'Precision')
+            ax2.set_title(f'Recall')
+            ax3.set_title(f'F-score')
+            fig.suptitle(f"{file_path.split('_')[2].split('.')[0]} - 3 sources - {int(inf)}% infection{' - Neighbors' if file_path.split('_')[-1].split('.')[0] == 'nb' else ''}")
+            plt.savefig(f"data/figures/comparison/comparison_3s_{int(inf)}inf_{file_path.split('_')[2].split('.')[0]}{'_nb' if file_path.split('_')[-1].split('.')[0] == 'nb' else ''}", dpi=120)
 
 
 if __name__ == '__main__':
